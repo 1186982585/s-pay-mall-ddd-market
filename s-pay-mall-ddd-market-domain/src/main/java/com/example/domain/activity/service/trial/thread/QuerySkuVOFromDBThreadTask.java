2d8ea1817488c5a481abbf9e3d3677357b313ea3
chuxiaojie <1186982585@qq.com>
2025-05-29 11:32:23 +0800

    feat: 多线程异步数据加载

---
+++ s-pay-mall-ddd-market-domain/src/main/java/com/example/domain/activity/service/trial/thread/QuerySkuVOFromDBThreadTask.java
@@ -0,0 +1,28 @@
+package com.example.domain.activity.service.trial.thread;
+
+import com.example.domain.activity.adapter.repository.IActivityRepository;
+import com.example.domain.activity.model.valobj.SkuVO;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @Author cxj
+ * @Date 2025/5/25 15:43
+ * @Description: 查询商品信息任务
+ */
+public class QuerySkuVOFromDBThreadTask implements Callable<SkuVO> {
+
+    private final String goodsId;
+
+    private final IActivityRepository activityRepository;
+
+    public QuerySkuVOFromDBThreadTask(String goodsId, IActivityRepository activityRepository) {
+        this.goodsId = goodsId;
+        this.activityRepository = activityRepository;
+    }
+
+    @Override
+    public SkuVO call() throws Exception {
+        return activityRepository.querySkuByGoodsId(goodsId);
+    }
+}
---
+++ s-pay-mall-ddd-market-infrastructure/src/main/java/com/example/infrastructure/dao/ISkuDao.java
@@ -0,0 +1,19 @@
+package com.example.infrastructure.dao;
+
+import com.example.infrastructure.dao.po.GroupBuyActivity;
+import com.example.infrastructure.dao.po.Sku;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author cxj
+ * @Date 2025/5/26 16:56 拼团活动Dao
+ * @Description:
+ */
+@Mapper
+public interface ISkuDao {
+
+    Sku querySkuByGoodsId(String goodsId);
+
+//    GroupBuyActivity queryValidGroupBuyActivity(GroupBuyActivity groupBuyActivityReq);
+
+}
